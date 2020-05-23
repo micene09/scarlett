a9b84cbe9b9216dd5b8da79e136e95ee62513008
micene09 <giovanni.contino09@gmail.com>
2020-05-23 19:34:52 +0200

    now using a wrapper

--- src/rest-options.ts
+++ src/rest-options.ts
@@ -1,30 +1,10 @@
-import { IRequestOptions, HttpResponseFormat, IResponseFilter, IQueryParamTransformer } from './interfaces';
-export class RestOptions implements IRequestOptions {
-	query?: { [key: string]: any };
-	queryParamsTransormer?: IQueryParamTransformer;
-	queryParamsIncludeEmpty?: boolean;
-	host?: string;
-	basePath?: string;
-	responseType?: HttpResponseFormat;
-	body?: ArrayBuffer | ArrayBufferView | Blob | File | string | URLSearchParams | FormData | {
-		[key: string]: any;
-	};
-	abortController?: AbortController;
-	keepalive?: boolean;
-	timeout?: number;
-	headers?: Headers;
-	useCache?: boolean;
-	cacheKey?: string;
-	throw?: boolean;
-	throwExcluding?: IResponseFilter<any>[];
+import { IRequestOptions } from './interfaces';
+export class RestOptions {
+	private current: IRequestOptions = {};
 	constructor(opts: IRequestOptions) {
-		this.assign(opts, this);
+		this.current = opts;
 	}
-	private assign(source: IRequestOptions, target: any) {
-		Object.assign(target, source);
-	}
-	public overrideWith(opts?: IRequestOptions | undefined) {
-		if (!opts) return;
-		this.assign(opts, this);
+	public localOverride(opts?: IRequestOptions | undefined) {
+		return Object.assign({}, this.current, opts ?? {});
 	}
 }
