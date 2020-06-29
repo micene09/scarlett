451ca481f657fce305266843b77bfe8737a9e0fd
micene09 <giovanni.contino09@gmail.com>
2020-06-29 15:02:40 +0200

    distributing rest options

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import RestClient from './rest-client';
 import RestError from './rest-error';
+import { RestOptions } from './rest-options';
 
 export default RestClient;
-export { RestError };
+export { RestOptions, RestError };
 export { IRequestOptions, IRequestQueryOptions, IResponse, IRequest, IResponseFilter, HttpMethod, HTTPStatusCode } from './interfaces';
