e98c03f6776ec4561d86436aeb1204c40c8bbfe6
micene09 <giovanni.contino09@gmail.com>
2021-11-23 19:20:33 +0100

    fixing statusCodes

--- src/rest-error.ts
+++ src/rest-error.ts
@@ -1,15 +1,16 @@
+import { HTTPStatusCode } from ".";
 import { IRequest, IResponseFilter } from "./interfaces";
 
 export default class RestError<TError> extends Error {
 	isRestError = true;
 	request?: IRequest;
 	fetchResponse?: Response;
-	statusCode: string | number = "";
+	statusCode: HTTPStatusCode | "timeout" | "body-parse-error" | null = null;
 	data?: TError;
-	constructor(statusCode: string | number, message: string) {
+	constructor(statusCode: HTTPStatusCode | "timeout" | "body-parse-error", message: string) {
 		super(message);
 		this.statusCode = statusCode;
-		this.message = `[${this.statusCode}] ${message}`;
+		this.message = message;
 	}
 	throwFilterMatch(flt: IResponseFilter<TError>): boolean {
 		if (!this.request || !this.statusCode) return false;
