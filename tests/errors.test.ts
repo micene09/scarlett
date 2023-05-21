01ce2bba0a0d6eb026f1a12b104b0ea9cd28a02a
Micene09 <giovanni.contino09@gmail.com>
2023-05-21 21:30:46 +0200

    test(rest-error): cover functional and class api

--- tests/errors.test.ts
+++ tests/errors.test.ts
@@ -1,61 +1,42 @@
-import RestClient, { RestError, RestOptions } from "../src/index";
-import { startWebServer, stopWebServer, ITestStatusCodeResponse } from "./runtime.setup";
+import type { RestError } from "../src/index";
+import { useTestServer, useTestRestClient, TestRestClient } from "./runtime.setup";
 import { beforeAll, afterAll, describe, test, expect, vi } from "vitest";
 import { fail, ok } from "assert";
 
-let baseClient: RestClient;
-let baseOptions: RestOptions;
-let host: string = "";
+let stopWebServer = () => {};
+let testServer = "";
 beforeAll(async () => {
-	host = await startWebServer();
-	baseOptions = new RestOptions()
-		.set("host", host)
-		.set("responseType", "json")
-		.set("throw", true);
-	baseClient = baseOptions.createRestClient();
+	const { host, stop } = await useTestServer();
+	testServer = host as string
+	stopWebServer = stop;
 });
-afterAll(() => {
-	stopWebServer();
-});
+afterAll(() => stopWebServer());
 
-describe('Rest errors', () => {
+describe('Rest Error using Functional API', () => {
 	test("Throw error on not successful requests", () => {
 
-		const baseOptions = new RestOptions()
-			.set("host", host)
-			.set("responseType", "json")
-			.set("throw", true)
-		const client = baseOptions.createRestClient()
-
-		expect(() => client.get("/status-code/500/empty"))
+		const { getStatusCodeEmpty } = useTestRestClient(testServer)
+		expect(() => getStatusCodeEmpty(500))
 			.rejects
 			.toThrowError();
 	})
 	test("Error will not be thrown, because of the onError callback", async () => {
 
 		const onErrorCallback = vi.fn(err => err)
-		const baseOptions = new RestOptions()
-			.set("host", host)
-			.set("responseType", "json")
-			.set("onError", onErrorCallback)
-			.set("throw", true)
-		const client = baseOptions.createRestClient()
-
-		await client.get("/status-code/500/empty");
+		const { getStatusCodeEmpty, setOption } = useTestRestClient(testServer);
+		setOption("onError", onErrorCallback);
+		await getStatusCodeEmpty(500);
 		expect(onErrorCallback).toBeCalled();
 	})
 	test("Throw error disabled, onError() callback will never be called", async () => {
 
 		const onErrorCallback = vi.fn(err => err)
-		const baseOptions = new RestOptions()
-			.set("host", host)
-			.set("responseType", "json")
-			.set("onError", onErrorCallback)
-			.set("throw", false)
-		const client = baseOptions.createRestClient()
+		const { getStatusCodeEmpty, setOption } = useTestRestClient(testServer);
+		setOption("onError", onErrorCallback);
+		setOption("throw", false);
 
 		try {
-			await client.get("/status-code/500/empty")
+			await getStatusCodeEmpty(500);
 			expect(onErrorCallback).not.toBeCalled()
 			ok("Error not thrown as expected.")
 		}
@@ -65,44 +46,35 @@ describe('Rest errors', () => {
 
 		const handledStatusCode = 502
 		const onErrorCallback = vi.fn(err => {})
-		const baseOptions = new RestOptions()
-			.set("host", host)
-			.set("responseType", "json")
-			.set("onError", onErrorCallback)
-			.set("throwExcluding", [ { statusCode: handledStatusCode } ])
-		const client = baseOptions.createRestClient()
+		const { getStatusCodeEmpty, setOption } = useTestRestClient(testServer);
+		setOption("onError", onErrorCallback);
+		setOption("throwExcluding", [ { statusCode: handledStatusCode } ]);
 
-		const response = await client.get(`/status-code/${handledStatusCode}/empty`)
-		expect(onErrorCallback).not.toBeCalled()
-		expect(response.status).toEqual(handledStatusCode)
-		expect(response.data).toBeNull()
+		const response = await getStatusCodeEmpty(handledStatusCode);
+		expect(onErrorCallback).not.toBeCalled();
+		expect(response.status).toEqual(handledStatusCode);
+		expect(response.data).toBeNull();
 
 		try {
-			await client.get(`/status-code/404/empty`)
-			fail("Error not thrown on unhandled (404) status code")
+			await getStatusCodeEmpty(404);
+			fail("Error not thrown on unhandled (404) status code");
 		}
 		catch {
 			expect(onErrorCallback).toBeCalled()
 		}
 	});
 	test("Throw error enabled, excluding errors via custom hook functions", async () => {
-		const baseOptions = new RestOptions()
-			.set("host", host)
-			.set("responseType", "json")
-		const client = baseOptions.createRestClient()
 
-		const businessApiCall = vi.fn((body: any) => new Promise<void>(resolve => {
-			setTimeout(resolve, 300);
-		}));
-		const businessApiTrigger = 405;
-
-		client.options.set("throwExcluding", [
+		const { getStatusCode, setOption } = useTestRestClient(testServer);
+		const whiteListedCall = vi.fn((body: any) => new Promise<void>(resolve => setTimeout(resolve, 100)));
+		const whiteListedStatusCode = 405;
+		setOption("throwExcluding", [
 			{ statusCode: 404 },
 			err => err.statusCode === 401,
-			async (err) => {
+			async err => {
 				let isMatchingBusinessLogic = false;
-				if (err.statusCode === businessApiTrigger) {
-					await businessApiCall(err.data);
+				if (err.statusCode === whiteListedStatusCode) {
+					await whiteListedCall(err.data);
 					isMatchingBusinessLogic = true;
 				}
 				return isMatchingBusinessLogic;
@@ -110,12 +82,12 @@ describe('Rest errors', () => {
 			{ statusCode: 409 }
 		]);
 
-		await client.get(`/status-code/404`)
-		await client.get(`/status-code/${businessApiTrigger}`)
-		await client.get(`/status-code/409`)
-		expect(businessApiCall).toBeCalledTimes(1)
+		await getStatusCode(404);
+		await getStatusCode(whiteListedStatusCode);
+		await getStatusCode(409);
+		expect(whiteListedCall).toBeCalledTimes(1)
 		try {
-			await client.get(`/status-code/500`)
+			await getStatusCode(500);
 			fail("Expected to throw error")
 		} catch (error) {
 			ok(error, "Error thrown as expected")
@@ -123,19 +95,132 @@ describe('Rest errors', () => {
 	});
 	test("Custom Error Object Interfaces", async () => {
 
-		const response = await baseClient.get<any, ITestStatusCodeResponse>("/status-code/412", { throw: false });
+		const { getStatusCode, setOption } = useTestRestClient(testServer);
+		setOption("throw", false);
+		const response = await getStatusCode(412);
 		const errorData = response?.error?.data;
 		// intellisense here should work data prop:
 		expect(errorData?.statusText).toEqual("CustomStatusCode");
 		expect(errorData?.statusCode).toEqual(412);
 	});
 	test("Custom Error Object handled as usual", async () => {
+		const { getStatusCode } = useTestRestClient(testServer);
 		try {
-			await baseClient.get("/status-code/412", { throw: true });
+			await getStatusCode(412);
 			fail();
 		}
 		catch(err) {
-			const error = err as RestError<ITestStatusCodeResponse>;
+			type Response = Awaited<ReturnType<typeof getStatusCode>>
+			type Error = Required<Response>["error"]
+			const error = err as Error;
+			expect(error.data?.statusText).toEqual("CustomStatusCode");
+			expect(error.data?.statusCode).toEqual(412);
+		}
+	});
+});
+describe('Rest Error using Class API', () => {
+	test("Throw error on not successful requests", () => {
+
+		const client = new TestRestClient(testServer)
+		expect(() => client.getStatusCodeEmpty(500))
+			.rejects
+			.toThrowError();
+	})
+	test("Error will not be thrown, because of the onError callback", async () => {
+
+		const onErrorCallback = vi.fn(err => err);
+		const client = new TestRestClient(testServer);
+		client.options.set("onError", onErrorCallback);
+
+		await client.getStatusCodeEmpty(500);
+		expect(onErrorCallback).toBeCalled();
+	})
+	test("Throw error disabled, onError() callback will never be called", async () => {
+
+		const onErrorCallback = vi.fn(err => err);
+		const client = new TestRestClient(testServer);
+		client.options.set("onError", onErrorCallback);
+		client.options.set("throw", false);
+
+		try {
+			await client.getStatusCodeEmpty(500);
+			expect(onErrorCallback).not.toBeCalled()
+			ok("Error not thrown as expected.")
+		}
+		catch(e) { fail(e as any); }
+	})
+	test("Throw error enabled, but will not throw on 'special' requests", async () => {
+
+		const handledStatusCode = 502
+		const onErrorCallback = vi.fn(err => {})
+		const client = new TestRestClient(testServer);
+		client.options.set("onError", onErrorCallback);
+		client.options.set("throw", false);
+		client.options.set("throwExcluding", [ { statusCode: handledStatusCode } ]);
+
+		const response = await client.getStatusCodeEmpty(handledStatusCode);
+		expect(onErrorCallback).not.toBeCalled();
+		expect(response.status).toEqual(handledStatusCode);
+		expect(response.data).toBeNull();
+
+		try {
+			await client.getStatusCodeEmpty(404);
+			fail("Error not thrown on unhandled (404) status code")
+		}
+		catch {
+			expect(onErrorCallback).toBeCalled()
+		}
+	});
+	test("Throw error enabled, excluding errors via custom hook functions", async () => {
+
+		const client = new TestRestClient(testServer);
+		const whiteListedCall = vi.fn((body: any) => new Promise<void>(resolve => setTimeout(resolve, 100)));
+		const whiteListedStatusCode = 405;
+		client.options.set("throwExcluding", [
+			{ statusCode: 404 },
+			err => err.statusCode === 401,
+			async err => {
+				let isMatchingBusinessLogic = false;
+				if (err.statusCode === whiteListedStatusCode) {
+					await whiteListedCall(err.data);
+					isMatchingBusinessLogic = true;
+				}
+				return isMatchingBusinessLogic;
+			},
+			{ statusCode: 409 }
+		]);
+
+		await client.getStatusCodeEmpty(404);
+		await client.getStatusCodeEmpty(whiteListedStatusCode);
+		await client.getStatusCodeEmpty(409);
+		expect(whiteListedCall).toBeCalledTimes(1)
+		try {
+			await client.getStatusCodeEmpty(500);
+			fail("Expected to throw error")
+		} catch (error) {
+			ok(error, "Error thrown as expected")
+		}
+	});
+	test("Custom Error Object Interfaces", async () => {
+
+		const client = new TestRestClient(testServer);
+		client.options.set("throw", false);
+		const response = await client.getStatusCode(412);
+		const errorData = response?.error?.data;
+		// intellisense here should work data prop:
+		expect(errorData?.statusText).toEqual("CustomStatusCode");
+		expect(errorData?.statusCode).toEqual(412);
+	});
+	test("Custom Error Object handled as usual", async () => {
+		const client = new TestRestClient(testServer);
+		try {
+			await client.getStatusCode(412);
+			fail();
+		}
+		catch(err) {
+			type Response = Awaited<ReturnType<typeof client.getStatusCode>>
+			type Error = Required<Response>["error"]
+			const error = err as Error;
 			expect(error.data?.statusText).toEqual("CustomStatusCode");
 			expect(error.data?.statusCode).toEqual(412);
 		}
