fe1c79ffdc6430107b63c6528e156e5a7a3c9e4e
Micene09 <giovanni.contino09@gmail.com>
2023-05-22 20:37:42 +0200

    test(rest-client-builder): dedicated builder test file

--- tests/rest-client.test.ts
+++ tests/rest-client.test.ts
@@ -1,5 +1,5 @@
 import { RestClientBuilder, useRestClientBuilder } from "../src";
-import { TestRestBuilder, TestRestClient, useTestRestClient, useTestServer } from "./runtime.setup";
+import { TestRestClient, useTestRestClient, useTestServer } from "./runtime.setup";
 import { beforeAll, afterAll, describe, test, expect, vi } from "vitest";
 
 let stopWebServer = () => {};
@@ -12,40 +12,6 @@ beforeAll(async () => {
 afterAll(() => stopWebServer());
 
 describe('Rest Client using Functional API', () => {
-	test("Rest client using builder", async () => {
-
-		const builder1 = useRestClientBuilder({
-			host: testServer,
-			responseType: "json",
-			throw: true
-		});
-		builder1.setOption("headers", new Headers({ "x-restoptions": "1" }));
-		const useRest1 = builder1.createRestClient();
-
-		const builder2 = useRestClientBuilder({
-			host: testServer,
-			responseType: "json",
-			throw: true
-		});
-		builder2.setOption("headers", new Headers({ "x-restoptions": "2" }));
-		const useRest2 = builder2.createRestClient();
-
-		const builder3 = useRestClientBuilder({
-			host: testServer,
-			responseType: "json",
-			throw: true
-		});
-		builder3.setOption("headers", new Headers({ "x-restoptions": "3" }));
-		const useRest3 = builder3.createRestClient();
-
-		const resp1 = await useRest1().get("/mirror");
-		const resp2 = await useRest2().get("/mirror");
-		const resp3 = await useRest3().get("/mirror");
-
-		expect(resp1.data?.headers["x-restoptions"]).toEqual("1");
-		expect(resp2.data?.headers["x-restoptions"]).toEqual("2");
-		expect(resp3.data?.headers["x-restoptions"]).toEqual("3");
-	});
 	test("Override global settings on local requests", async () => {
 
 		const { mirror } = useTestRestClient(testServer);
@@ -109,43 +75,6 @@ describe('Rest Client using Functional API', () => {
 	});
 });
 describe('Rest Client using Class API', () => {
-	test("Rest client using builder", async () => {
-
-		const builder1 = new RestClientBuilder<any, any, TestRestClient>({
-			host: testServer,
-			responseType: "json",
-			throw: true
-		});
-		builder1.setFactory(TestRestClient);
-		builder1.set("headers", new Headers({ "x-restoptions": "1" }));
-		const restOpts1 = builder1.createRestClient<typeof TestRestClient>(testServer)
-
-		const builder2 = new RestClientBuilder<any, any, TestRestClient>({
-			host: testServer,
-			responseType: "json",
-			throw: true
-		});
-		builder2.set("headers", new Headers({ "x-restoptions": "2" }));
-		builder2.setFactory(TestRestClient);
-		const restOpts2 = builder2.createRestClient<typeof TestRestClient>(testServer);
-
-		const builder3 = new RestClientBuilder<any, any, TestRestClient>({
-			host: testServer,
-			responseType: "json",
-			throw: true
-		});
-		builder3.set("headers", new Headers({ "x-restoptions": "3" }));
-		builder3.setFactory(TestRestClient);
-		const restOpts3 = builder3.createRestClient(testServer);
-
-		const resp1 = await restOpts1.mirror("GET");
-		const resp2 = await restOpts2.mirror("GET");
-		const resp3 = await restOpts3.mirror("GET");
-
-		expect(resp1.data?.headers["x-restoptions"]).toEqual("1");
-		expect(resp2.data?.headers["x-restoptions"]).toEqual("2");
-		expect(resp3.data?.headers["x-restoptions"]).toEqual("3");
-	});
 	test("Override global settings on local requests", async () => {
 
 		const baseClient = new TestRestClient(testServer);
