d0e924fd650b59b33f3f785b76bef5bcdc12393c
Micene09 <giovanni.contino09@gmail.com>
2023-07-01 09:08:33 +0200

    test: code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	test: {
 		environment: "node",
-		testTimeout: 120000
+		testTimeout: 120000,
+		coverage: {
+			provider: "c8",
+			reporter: ["json", "text"]
+		}
 	}
 });
