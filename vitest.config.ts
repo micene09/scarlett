0711f3bdd1f8e417f506a0d43d413d6baad6dc1a
Micene09 <giovanni.contino09@gmail.com>
2024-01-09 01:46:49 +0100

    test: vitest setup file

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	test: {
 		environment: "node",
+		setupFiles: "./vitest.setup.ts",
 		testTimeout: 120000,
 		coverage: {
 			provider: "v8",
